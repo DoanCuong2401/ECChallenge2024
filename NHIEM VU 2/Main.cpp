0381016d5eda195bba0612b9b64457de89f4e1e3
DoanCuong2401 <doancuong2416@gmail.com>
2024-09-19 00:12:13 +0700

    Nv2.cpp

--- NHIEM VU 2/Main.cpp
+++ NHIEM VU 2/Main.cpp
@@ -1,1 +1,73 @@
-// Main
+#include <Leanbot.h>                    // use Leanbot library
+
+
+void setup() {
+  Leanbot.begin();                      // initialize Leanbot
+}
+
+void Buzzer() {
+  Leanbot.tone(1000, 5000);
+}
+
+void EmergencySignal() {
+  byte LedRandom = BITMAP(ledA, ledF, ledE, ledD, ledC, ledB);
+  LbRGB[LedO] = CRGB::Yellow;
+  LbRGB.show();
+  while(1) {
+    LbRGB.fillColor(CRGB::Red, LedRandom);
+    LbDelay(1000);
+    LbRGB.show();
+  }
+}
+
+void Out_of_CO2() {
+  LbMotion.runLR(100, 100);
+  LbMotion.waitDistanceMm(600);
+  LbDelay(1000);
+  LbMotion.runLR(-50, 50);
+  LbMotion.waitRotationDeg(90);
+  LbDelay(1000);
+  LbMotion.runLR(100, 100);
+  LbMotion.waitDistanceMm(900);
+}
+
+void Motion() {
+  int line = LbIRLine.read();  
+  static int count = 0;       
+
+  if (line == 0b1100) {  
+    LbMotion.runLR(-50, 50);   
+    LbMotion.waitRotationDeg(90);
+    LbMotion.runLR(100, 100);  
+  } 
+  else if (line == 0b0011) {  
+    LbMotion.runLR(50, -50);   
+    LbMotion.waitRotationDeg(90);
+    LbMotion.runLR(100, 100);  
+  } 
+  else if (line == 0b0011 && line == 0b1100) { 
+    if (count == 0) {  
+      LbMotion.runLR(-50, 50);
+      LbMotion.waitRotationDeg(90);
+      LbMotion.runLR(100, 100);
+      count++;
+    } else { 
+      LbMotion.runLR(100, 100);
+    }
+  } 
+  else {  
+    LbMotion.runLR(100, 100);
+  }
+}
+
+void FireZone() {
+  int zone = LBIRLine.read();
+  if(zone == 0) {
+    LbGripper.open();
+    Buzzer();
+  } else {
+    LbGripper.close();
+  }
+}
+
+
